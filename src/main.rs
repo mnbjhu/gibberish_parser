96314d65d468eb61660f432f55ab063a1ce0c951
James Gibson <james-gibson@ntlworld.com>
2025-11-26 17:49:11 +0000

    cleanup

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-use gibberish_gibberish_parser::Gibberish;
-
-pub fn main() {
-    let file = fs::read_to_string("test.gibtest").unwrap();
-    let res = Gibberish::parse(&file);
-    res.debug_print(true, true, &Gibberish);
-}
